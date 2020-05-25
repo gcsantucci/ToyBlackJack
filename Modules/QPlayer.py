a08b1c3eb350ed6218e84a914f9bec6769693daa
Gabriel Santucci <gabrielsantucci@gmail.com>
2020-05-25 15:23:25 -0400

    QPlayer now save/load Qtables, performance still poor

--- Modules/QPlayer.py
+++ Modules/QPlayer.py
@@ -4,14 +4,19 @@ from Players import Player
 
 class QPlayer(Player):
     # https://towardsdatascience.com/simple-reinforcement-learning-q-learning-fcddc4b6fe56
-    def __init__(self, stack=10_000, count=0, eps=0.2, alpha=0.1, gamma=0.9):
+    def __init__(self, stack=10_000, count=0, eps=0.2, alpha=0.1, gamma=0.9, load=False, update=True):
         Player.__init__(self, name='Q', stack=stack, count=count)
-        self.Q = np.zeros((10, 20, 2))
         self.eps = eps  # percentage of exploration
         self.alpha = alpha  # learning rate
         self.gamma = gamma  # discount factor, typically 0.8-0.99
         self.actions = [0, 1]  # 0: Stand, 1: Hit
         self.offset = 3
+        self.load = load
+        self.update = update
+        if self.load:
+            self.Q = np.load(self.load)
+        else:
+            self.Q = np.zeros((10, 20, 2))
 
     def ExploreOrExploit(self):
         d, t = self.GetState()
@@ -32,10 +37,6 @@ class QPlayer(Player):
         self.Q[d, s, a] = (1 - self.alpha) * self.Q[d, s, a] + \
             self.alpha * (reward + self.gamma * np.max(self.Q[d, ns, :]))
 
-    def SetBet(self, ncards):
-        tcount = self.GetTrueCount(ncards)
-        self.bet = 100
-
     def TakeAction(self, dealer):
         if self.GetTotal() > 21:
             return self.actions[0]
@@ -44,6 +45,8 @@ class QPlayer(Player):
         return self.ExploreOrExploit()
 
     def ActionResponse(self, action):
+        if not self.update:
+            return
         if action == 0:
             new_state = self.s
         else:
@@ -55,7 +58,7 @@ class QPlayer(Player):
             reward = 20. if action == 0 else -10.
         else:
             reward = 5 if action == 1 else -5
-
         self.Update(reward, action, new_state)
 
-    def Save(self, name)
+    def Save(self, name='qtables/qtable.npy'):
+        np.save(name, self.Q)
